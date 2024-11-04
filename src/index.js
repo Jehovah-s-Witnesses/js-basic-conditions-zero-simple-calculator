e5217f6e674a2bc2ebe59ea063a95a5654d21755
gribVolnushka <burgersqueen98@gmail.com>
2024-11-05 00:27:32 +0200

    created simple calculator

---
+++ src/index.js
@@ -0,0 +1,61 @@
+const firstNumber = +prompt("Enter your first number");
+const operation = prompt("Enter your operation: addition (+), subtraction (-), multiplication (*), division (/) or exponentiation (**)");
+const secondNumber = +prompt("Enter your second number");
+let result;
+let roundNumber;
+let roundResult;
+
+if (!(isNaN(firstNumber)) && !(isNaN(secondNumber))) {
+  const userQuestion = confirm("Would you like to round the result?");
+  if (operation === `+`) {
+    result = firstNumber + secondNumber;
+    if (userQuestion) {
+      roundNumber = prompt("How many digits should there be after the decimal point?");
+      roundResult = result.toFixed(roundNumber);
+      alert(roundResult);
+    } else {
+      alert (result);
+    }
+  } else if (operation === `-` ) {
+    result = secondNumber - firstNumber;
+    if (userQuestion) {
+      roundNumber = prompt("How many digits should there be after the decimal point?");
+      roundResult = result.toFixed(roundNumber);
+      alert(roundResult);
+    } else {
+      alert (result);
+    }
+  } else if (operation === `*`) {
+    result = firstNumber * secondNumber;
+    if (userQuestion) {
+      roundNumber = prompt("How many digits should there be after the decimal point?");
+      roundResult = result.toFixed(roundNumber);
+      alert(roundResult);
+    } else {
+      alert (result);
+    }
+  } else if (operation === `/`) {
+    result = firstNumber / secondNumber;
+    if (userQuestion) {
+      roundNumber = prompt("How many digits should there be after the decimal point?");
+      roundResult = result.toFixed(roundNumber);
+      alert(roundResult);
+    } else {
+      alert (result);
+    }
+  } else if (operation === `**`) {
+    result = firstNumber ** secondNumber;
+    if (userQuestion) {
+      roundNumber = prompt("How many digits should there be after the decimal point?");
+      roundResult = result.toFixed(roundNumber);
+      alert(roundResult);
+    } else {
+      alert (result);
+    }
+  } else {
+    alert ("You entered invalid operation!")
+  }
+} else {
+  alert ("You entered an invalid value!");
+}
+
