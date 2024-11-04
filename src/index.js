bae202759c6a1396439fc2a4cb5bd144b75a0578
gribVolnushka <burgersqueen98@gmail.com>
2024-11-05 00:43:03 +0200

    created simple calculator

--- src/index.js
+++ src/index.js
@@ -1,61 +1,72 @@
-const firstNumber = +prompt("Enter your first number");
-const operation = prompt("Enter your operation: addition (+), subtraction (-), multiplication (*), division (/) or exponentiation (**)");
-const secondNumber = +prompt("Enter your second number");
+const firstNumber = +prompt('Enter your first number');
+const operation = prompt(
+  'Enter your operation: addition (+), subtraction (-), multiplication (*), division (/) or exponentiation (**)',
+);
+const secondNumber = +prompt('Enter your second number');
 let result;
 let roundNumber;
 let roundResult;
 
-if (!(isNaN(firstNumber)) && !(isNaN(secondNumber))) {
-  const userQuestion = confirm("Would you like to round the result?");
-  if (operation === `+`) {
+if (!isNaN(firstNumber) && !isNaN(secondNumber)) {
+  const isNeedToRound = confirm('Would you like to round the result?');
+  if (operation === '+') {
     result = firstNumber + secondNumber;
-    if (userQuestion) {
-      roundNumber = prompt("How many digits should there be after the decimal point?");
+    if (isNeedToRound) {
+      roundNumber = prompt(
+        'How many digits should there be after the decimal point?',
+      );
       roundResult = result.toFixed(roundNumber);
       alert(roundResult);
     } else {
-      alert (result);
+      alert(result);
     }
-  } else if (operation === `-` ) {
+  } else if (operation === '-') {
     result = secondNumber - firstNumber;
-    if (userQuestion) {
-      roundNumber = prompt("How many digits should there be after the decimal point?");
+    if (isNeedToRound) {
+      roundNumber = prompt(
+        'How many digits should there be after the decimal point?',
+      );
       roundResult = result.toFixed(roundNumber);
       alert(roundResult);
     } else {
-      alert (result);
+      alert(result);
     }
   } else if (operation === `*`) {
     result = firstNumber * secondNumber;
-    if (userQuestion) {
-      roundNumber = prompt("How many digits should there be after the decimal point?");
+    if (isNeedToRound) {
+      roundNumber = prompt(
+        'How many digits should there be after the decimal point?',
+      );
       roundResult = result.toFixed(roundNumber);
       alert(roundResult);
     } else {
-      alert (result);
+      alert(result);
     }
   } else if (operation === `/`) {
     result = firstNumber / secondNumber;
-    if (userQuestion) {
-      roundNumber = prompt("How many digits should there be after the decimal point?");
+    if (isNeedToRound) {
+      roundNumber = prompt(
+        'How many digits should there be after the decimal point?',
+      );
       roundResult = result.toFixed(roundNumber);
       alert(roundResult);
     } else {
-      alert (result);
+      alert(result);
     }
   } else if (operation === `**`) {
     result = firstNumber ** secondNumber;
-    if (userQuestion) {
-      roundNumber = prompt("How many digits should there be after the decimal point?");
+    if (isNeedToRound) {
+      roundNumber = prompt(
+        'How many digits should there be after the decimal point?',
+      );
       roundResult = result.toFixed(roundNumber);
       alert(roundResult);
     } else {
-      alert (result);
+      alert(result);
     }
   } else {
-    alert ("You entered invalid operation!")
+    alert('You entered invalid operation!');
   }
 } else {
-  alert ("You entered an invalid value!");
+  alert('You entered an invalid value!');
 }
-
